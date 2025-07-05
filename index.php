deff83398b7e92894cd5efb1004211221d599ed6
Susan Laime Lucero <160919532+SusanLaime@users.noreply.github.com>
2025-07-05 17:29:04 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="refresh" content="1;url=eng/index.php">
+        <meta name="Description" content ="The IEEE World Engineering Education Conference - EDUNINE is the flagship annual conference for the IEEE Region 9 (Latin America and the Caribbean) of the IEEE Education Society. The EDUNINE international conference on Education in Engineering, Technology, Computer Science, and related topics, held every year in March in a different Latin American city,  is organized by  COPEC - Science and Education Research Organization. The conference program covers the main issues present in education today.">
+        <meta name ="Keywords" content="IEEE, COPEC, Education in Engineering, Technology, and Computer Science, STEM, Teaching, Learning, Higher Education, University Education, Educación en Ingeniería, Tecnología e Informática, Enseñanza, Aprendizaje, Educacion Superior, Educación Universitaria, Educação em Engenharia, Tecnologia e Informática, Ensino, Aprendizagem, Ensino Superior, Educação Universitária, Latin America and the Caribbean, Latinoamérica y el Caribe, América Latina e Caribe, Conference, Congress, Conferencia, Congreso, Conferência, Congresso">
+        <meta name="author" content="COPEC">
+        <meta name="copyright" content="COPEC">
+        <title> EDUNINE2025 English Website:  Home </title>
+
+        <!-- Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+       
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+
+        <link href="../css/EDU9Conferences.css" rel="stylesheet"> 
+        <link href="../css/edu9parts.css" rel="stylesheet"> 
+    
+        <!-- Custom styles for this template -->
+        <script src="https://kit.fontawesome.com/5dc88a8201.js" crossorigin="anonymous"></script>
+
+    </head>
+    <body>
+        <?php
+         echo "<p> Loading webpage </p>";
+        ?>
+    </body>
+</html>
